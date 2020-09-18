e447881bcbe69568bdac89023dd182024c1f79fa
Nathan Andrade <nathandulino2003@gmail.com>
2020-09-17 23:22:08 -0300

    app clone

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState(['hi']);
+
+  // função para enviar as menssagens
+  const sendMessage = (event) => {
+    setMessages([...messages, input])
+    setInput('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Facebook Messenger Clone</h1>
+
+      {/* input field */}
+      <input value={input} onChange={event => setInput(event.target.value)} />
+
+      {/* button */}
+      <button onClick={sendMessage}>Send Message</button>
+
+      {/* messages themselves */}
+      {
+        messages.map(message => (
+          <p>{message}</p>
+        ))
+      }
     </div>
   );
 }
