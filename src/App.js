474be698a878b10f42992399211d45c4939fe82e
Nathan Andrade <nathandulino2003@gmail.com>
2020-09-18 23:22:29 -0300

    app clone

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
+
+import Message from './components/Messages/Message';
+
 import './App.css';
 
 function App() {
+
   const [input, setInput] = useState('');
   const [messages, setMessages] = useState(['hi']);
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+   const name = prompt('Please enter your name');
+  }, [])
 
   // função para enviar as menssagens
   const sendMessage = (event) => {
+    event.preventDefault();
     setMessages([...messages, input])
     setInput('');
   }
@@ -15,16 +26,18 @@ function App() {
     <div className="App">
       <h1>Facebook Messenger Clone</h1>
 
-      {/* input field */}
-      <input value={input} onChange={event => setInput(event.target.value)} />
-
-      {/* button */}
-      <button onClick={sendMessage}>Send Message</button>
+      <form>
+        <FormControl>
+          <InputLabel>Enter a message...</InputLabel>
+          <Input value={input} onChange={event => setInput(event.target.value)} />
+          <Button disabled={!input} variant="contained" color="primary" type="submit" onClick={sendMessage}>Send Message</Button>
+        </FormControl>
+      </form>
 
       {/* messages themselves */}
       {
         messages.map(message => (
-          <p>{message}</p>
+          <Message text={message} />
         ))
       }
     </div>
